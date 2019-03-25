af37db455a9e12cf08f5ae1bb729bfa2fc2dd57c
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-25 05:19:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :side_one, :side_two, :side_three
+  
+  def initialize(side_one, side_two, side_three)
+    if side_one + side_two < side_three || side_two + side_three < side_one || side_one + side_three < side_two
+      raise TriangleError
+    end 
+    if side_one <= 0 || side_two <= 0 || side_three <= 0
+      raise TriangleError
+    end 
+    if side_one == nil || side_two == nil || side_three == nil
+      raise TriangleError
+    end 
+    
+    @side_one = side_one
+    @side_two = side_two 
+    @side_three = side_three 
+  end 
+    
+  def kind 
+    if@side_one == @side_two && @side_three == @side_one
+      return :equilateral 
+    elsif @side_one == @side_two || @side_two == @side_three || @side_three == @side_one 
+      return :isosceles
+    elsif @side_one != @side_two && @side_two != @side_three && @side_three != @side_one 
+      return :scalene 
+  end 
+end
+
+class TriangleError < StandardError 
+    def message
+      "Invalid Triangle!"
+    end
 end
