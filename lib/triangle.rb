99bebd51b8530d423214ac8bf2846c656b1e467e
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-25 05:23:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,23 +4,14 @@ class Triangle
   attr_reader :side_one, :side_two, :side_three
   
   def initialize(side_one, side_two, side_three)
-    if side_one + side_two < side_three || side_two + side_three < side_one || side_one + side_three < side_two
-      raise TriangleError
-    end 
-    if side_one <= 0 || side_two <= 0 || side_three <= 0
-      raise TriangleError
-    end 
-    if side_one == nil || side_two == nil || side_three == nil
-      raise TriangleError
-    end 
-    
     @side_one = side_one
     @side_two = side_two 
     @side_three = side_three 
   end 
     
   def kind 
-    if@side_one == @side_two && @side_three == @side_one
+    valid? 
+    if @side_one == @side_two && @side_three == @side_one
       return :equilateral 
     elsif @side_one == @side_two || @side_two == @side_three || @side_three == @side_one 
       return :isosceles
