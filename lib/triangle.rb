b424ed8d5736acd5b425d2ed6071e38c461c0d6d
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-25 05:34:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end 
   
   def is_valid?
-    if (side_one + side_two) < side_three || (side_two + side_three) < side_one || (side_one + side_three) < side_two
+    if (side_one + side_two) <= side_three || (side_two + side_three) <= side_one || (side_one + side_three) <= side_two
       raise TriangleError
     elsif side_one <= 0 || side_two <= 0 || side_three <= 0
       raise TriangleError
