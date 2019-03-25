cb135a8b5e2ae133e64cbb9a45a3c7bd8ef10d76
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-25 05:26:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Triangle
-  attr_reader :side_one, :side_two, :side_three
+  attr_accessor :side_one, :side_two, :side_three
   
   def initialize(side_one, side_two, side_three)
     @side_one = side_one
